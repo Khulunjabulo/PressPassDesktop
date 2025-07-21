ecbd94cd291617c539162c6eeaf696344a99b305
ChristopherTsh <christophetshoma0045@gmail.com>
2025-07-21 14:39:49 +0200

    Update Firebase configuration to use environment variables

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp, getApps } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCknCZl-gQb8gD3msAbSf2vh28-Sb9FwZ4",
-  authDomain: "press-pass-98801.firebaseapp.com",
-  projectId: "press-pass-98801",
-  storageBucket: "press-pass-98801.appspot.com",
-  messagingSenderId: "899909510336",
-  appId: "1:899909510336:web:a3e32e5effaa9f591a3925",
-}
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
 const auth = getAuth(app)
