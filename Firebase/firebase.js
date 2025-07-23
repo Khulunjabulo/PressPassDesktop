3a4dab38f95d4cde380f4ef3a274a4dfba165cdd
ChristopherTsh <christophertshoma@gmail.com>
2025-07-23 14:12:34 +0200

    `Update Firebase configuration to use DATABASE_URL instead of hardcoded URL`

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -9,7 +9,7 @@ try {
   const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: 'https://press-pass-7c6f6-default-rtdb.firebaseio.com', 
+    databaseURL: DATABASE_URL, 
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
