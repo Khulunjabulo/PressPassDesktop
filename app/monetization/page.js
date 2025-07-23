da1b5fc89e5c22cd86cb396022f0a06ba39f6c20
Otsile <otsilekatane@gmail.com>
2025-07-23 15:53:22 +0200

    Monetization and wallet pages and routing

---
+++ app/monetization/page.js
@@ -0,0 +1,11 @@
+"use client"
+import Header from '@/components/UI/Header'
+
+export default function monetization() {
+  return (
+    <div>
+        <Header />
+  <div>Welcome to the  Page</div>
+  </div>
+  )
+}
